689cfabc12397d03f03c04384f7b4e979c615dbb
ambikarao <63193852+ambikarao@users.noreply.github.com>
2025-08-14 16:03:20 +0530

    Modified code for 2 files: /src/index.tsx, /src/reportWebVitals.ts

---
+++ src/reportWebVitals.test.ts
@@ -0,0 +1,7 @@
+import reportWebVitals from './reportWebVitals';
+
+describe('reportWebVitals', () => {
+  test('is a function', () => {
+    expect(typeof reportWebVitals).toBe('function');
+  });
+});
